59513d96e1df4f03629a9b8b14ab5abd7cc43e30
Nat <nat.wrw@gmail.com>
2020-04-29 12:24:12 +0700

    add missing dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version="1.0",
 	py_modules=["influx2csv"],
 	include_package_data=True,
-	install_requires=["click", "tqdm", "pandas"],
+	install_requires=["click", "tqdm", "pandas", "influxdb"],
 	entry_points="""
         [console_scripts]
         influx2csv=influx2csv:cli
