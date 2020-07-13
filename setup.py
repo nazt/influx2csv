5d4b3fdcb670195ebeb31766f1afc31df7a15c09
Nat Weerawan <nat@cmmc.io>
2020-07-13 16:59:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
 	version="1.0",
 	py_modules=["influx2csv"],
 	include_package_data=True,
+	author='Nat Weerawan',
+	author_email='nat.wrw@gmail.com',
+	license='MIT',
+	packages=['Nat'],	
 	install_requires=["click", "tqdm", "pandas", "influxdb"],
 	entry_points={
 		'console_scripts': ['influx2csv=influx2csv:cli'],
