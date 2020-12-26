b39b6ef55472e5b0e98d35b950ff692d974e753f
Nat <nat.wrw@gmail.com>
2020-12-26 14:06:30 +0700

    1.11

--- influx2csv/command_line.py
+++ influx2csv/command_line.py
@@ -94,6 +94,7 @@ def alldbs():
                     results.append(dct)
             else:
                 rs = client.query('show tag keys')
+                print(list(rs))
                 tag_keys = [i['tagKey'] for i in list(rs)]
                 if 'nickname' in tag_keys:
                     dct = {'db': db, 'measurement': measurement['name'], 'topic': False,
